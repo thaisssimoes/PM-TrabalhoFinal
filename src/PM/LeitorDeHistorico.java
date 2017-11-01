cc69f0fb75e53379fe4e7f20e3bc043bae218376
Thais Simoes <thais.simoes@uniriotec.br>
2017-11-01 15:11:05 -0200

    Lista de disciplinas com códigos e documentação inicial

--- src/PM/LeitorDeHistorico.java
+++ src/PM/LeitorDeHistorico.java
@@ -6,63 +6,98 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Classe destinada a ler, refinar e interpretar o histórico escolar do aluno
+ * 
+ * @author grupoPM
+ * 
+ * */
 
 public class LeitorDeHistorico {
 
 
 	    public static void main(String[] args) throws IOException{
-	    	ArrayList<String> indexesComecoEFimDasMaterias = new ArrayList<String>(); //melhorar isso
-	    	String indexComeco;
-	    	String indexFim;
+	    	ArrayList<Integer> indexesComecoEFimDasMaterias = new ArrayList<Integer>(); 
+	    	int indexComeco;
+	    	int indexFim;
 	    	
 	    	
 	    	File historicoEscolarCaminho = new File("C:/Users/tsuba/Documents/Faculdade/historicoEscolarCRAprovados.pdf");        
 	    	String historicoEscolarExtraido= extrairPDF(historicoEscolarCaminho);
 	    	
-	    	indexesComecoEFimDasMaterias = buscadoresDeIndex(historicoEscolarExtraido); //melhorar isso
+	    	indexesComecoEFimDasMaterias = buscadoresDeIndex(historicoEscolarExtraido); 
 	    	
 	    	indexComeco = indexesComecoEFimDasMaterias.get(0);
 	    	indexFim = indexesComecoEFimDasMaterias.get(1);
 	    	
 	    	
-	    	String historicoEscolarRefinado = refinadorDeConteudoDoHistoricoEscolar(historicoEscolarExtraido, Integer.valueOf(indexComeco), Integer.valueOf(indexFim));
+	    	String historicoEscolarRefinado = refinadorDeConteudoDoHistoricoEscolar(historicoEscolarExtraido,indexComeco, indexFim);
 	    	
 	    	
 	    	
 	    	System.out.print(historicoEscolarRefinado);
 	    
 	    }
+	    
+	    /**
+	     * 
+	     * Este método procura extrair o histórico escolar do documento PDF e transformá-lo em uma String
+	     * 
+	     * @param historicoEscolarCaminho (File) : É o caminho de onde o histórico está armazenado na máquina. 
+	     * 
+	     * @return String com o histórico extraido
+	     * 
+	     * */
 
 	    public static String extrairPDF(File historicoEscolarCaminho) throws IOException {
 	        PDDocument historicoEscolar = PDDocument.load(historicoEscolarCaminho);        
 	        PDFTextStripper pdfStripper = new PDFTextStripper();
-	        String text = pdfStripper.getText(historicoEscolar);
+	        String historicoEscolarExtraido = pdfStripper.getText(historicoEscolar);
 	        historicoEscolar.close();
-	        return text;
+	        return historicoEscolarExtraido;
 	    }
 	    
-	    //MELHORAR ISSO!!
-	    public static ArrayList<String> buscadoresDeIndex (String historicoEscolarLido) throws IOException {
-	    	ArrayList<String> indexesComecoEFimDasMaterias = new ArrayList<String>();
+	    /**
+	     * Este método procura encontrar o início e o fim aproximado do conteúdo de aprovação do histórico escolar.
+	     *  
+	     * @param historicoEscolarExtraido (String): String com o histórico escolar completo, antes do refinamento
+	     * 
+	     * @return : ArrayList<Integer> com os valores correspondentes às posições inicial e final do bloco de aprovações
+	     * 
+	     * */
+	        
+	    public static ArrayList<Integer> buscadoresDeIndex (String historicoEscolarExtraido) throws IOException {
+	    	ArrayList<Integer> indexesComecoEFimDasMaterias = new ArrayList<Integer>();
 
-	    	String indexComeco = Integer.toString(historicoEscolarLido.indexOf("Situação Local"));
-	    	String indexFim = Integer.toString(historicoEscolarLido.indexOf("Coeficiente de Rendimento Geral"));
+	    	int indexComeco = historicoEscolarExtraido.indexOf("Situação Local");
+	    	int indexFim = historicoEscolarExtraido.indexOf("Coeficiente de Rendimento Geral");
 	    	
 	    	
 	    	indexesComecoEFimDasMaterias.add(indexComeco);
 	    	indexesComecoEFimDasMaterias.add(indexFim);
 	    	
 	    	return indexesComecoEFimDasMaterias;
-	    }
+	    }	    
 	    
-	    public static String refinadorDeConteudoDoHistoricoEscolar(String historicoEscolarLido, int indexComeco, int indexFim){
-	    	
-	    	
-	    	String historicoEscolarRefinado= historicoEscolarLido.substring(indexComeco, indexFim);   	
-	    	
+	    /**
+	     * Este método se destina a separar o bloco de aprovações do restante das informações do histórico
+	     * 
+	     * @param historicoEscolarExtraido (String) : String com o histórico escolar completo, antes do refinamento
+	     * @param indexComeco (Int) : Indicador do começo do texto refinado
+	     * @param indexFim (Int) : Indicador do fim do texto refinado
+	     * 
+	     * @return Substring com o bloco de matérias separado do restante
+	     * 
+	     * */
+	    
+	    public static String refinadorDeConteudoDoHistoricoEscolar(String historicoEscolarExtraido, int indexComeco, int indexFim){
+	  	    	
+	    	String historicoEscolarRefinado= historicoEscolarExtraido.substring(indexComeco, indexFim);   		    	
 	    	return historicoEscolarRefinado;
 	    }
 
+	    
+	    
    }
 	
 
