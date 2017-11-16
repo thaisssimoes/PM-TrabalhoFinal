72b34ed0deddb9351f38fb27ea89c36fa0b250cb
Yurilopam <yurilopam@gmail.com>
2017-11-16 14:57:52 -0200

    Migração para Maven e refactor de metodos

---
+++ projeto-pm/src/main/java/Disciplina.java
@@ -0,0 +1,107 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Classe para reconhecimento de informações referentes às disciplinas no Historico Escolar. 
+ * 
+ * @author grupoPM
+ * 
+ * */
+
+public class Disciplina {
+
+	
+	/**
+	 * Atributos da classe
+	 * 
+	 * */	
+	
+//	private int initialSize = 16; //Tamanho Inicial da Lista
+//	private double loadFactor = 0.75; //Valor do Load Factor	
+//	private double sizeToRehash = initialSize * loadFactor;	
+	private static Map<String,String> informacaoesDeDisciplinas = new HashMap<String,String>();
+	private static Scanner leitorDeCodigo;
+	private static Scanner leitorDeHistorico;
+	
+	/**
+	 * 
+	 * Este método lê e armazena em hashmap uma lista .txt de disciplinas e códigos que será usada posteriormente. 
+	 * 
+	 * @param caminhoLista(String) : endereço da lista na máquina
+	 * 
+	 * */
+	
+	public static void importarListaDisciplinas(String caminhoLista) throws IOException{
+		String codigoDisciplina;
+		String nomeDisciplina;
+		String disciplinaRetiradaDaLista;
+		
+		File listaDisciplina = new File(caminhoLista);
+		String[] arrayDeCodigoENome; 
+		
+		try {
+		        Scanner leitorDisciplinas = new Scanner(listaDisciplina);
+		        while (leitorDisciplinas.hasNextLine()) {		            
+		        	disciplinaRetiradaDaLista = leitorDisciplinas.nextLine();		        	
+		        	arrayDeCodigoENome = disciplinaRetiradaDaLista.split(":");		           		        	
+		        	codigoDisciplina = arrayDeCodigoENome[0];
+		            nomeDisciplina = arrayDeCodigoENome[1];	            		            
+		            informacaoesDeDisciplinas.put(codigoDisciplina, new String(nomeDisciplina));		        	
+		        }
+		        leitorDisciplinas.close();	        	
+		    } 
+		    catch (IOException e) {
+		        e.printStackTrace();
+		  }
+	}
+	
+	
+	
+	/**
+	 * Este método procura encontrar o status de aprovação do aluno em uma disciplina e armazená-lo no hashmap
+	 * 
+	 * @param historicoRefinado (String) : um bloco menor [apenas com as disciplinas] do historico escolar
+	 * 
+	 * */
+		
+	public static void encontrarStatusDeAprovacao(String historicoRefinado){
+        
+		leitorDeHistorico = new Scanner(historicoRefinado);
+		String codigo;
+		
+		while (leitorDeHistorico.hasNextLine()) {            
+			String linhaAtual = leitorDeHistorico.nextLine();
+			leitorDeCodigo = new Scanner(linhaAtual);
+			codigo = leitorDeCodigo.next();		
+			for (String codigoChave : informacaoesDeDisciplinas.keySet()) {
+				if(codigo.equals(codigoChave) && !codigo.equals("HTD0058")){
+					String[] separadorDeStatus = linhaAtual.split("-");
+					if(codigo.equals("TIN0110")){
+			            informacaoesDeDisciplinas.put(codigoChave, new String(separadorDeStatus[2]));		        	
+					}
+					else{
+						informacaoesDeDisciplinas.put(codigoChave, new String(separadorDeStatus[1]));
+					}								
+				}
+				else if(codigo.equals(codigoChave) && codigo.equals("HTD0058")){
+					linhaAtual = leitorDeHistorico.nextLine();
+					linhaAtual = leitorDeHistorico.nextLine();
+					String[] separadorDeStatus = linhaAtual.split("-");
+					informacaoesDeDisciplinas.put(codigoChave, new String(separadorDeStatus[1]));
+					
+				}
+			
+			}
+	
+		}
+
+	}
+
+}
+
+
+	
+
---
+++ projeto-pm/src/main/java/LeitorDeHistorico.java
@@ -0,0 +1,151 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+/**
+ * Classe destinada a ler, refinar e interpretar o histórico escolar do aluno
+ * 
+ * @author grupoPM
+ * 
+ * */
+
+public class LeitorDeHistorico {
+
+
+		private static final String LISTA_DE_DISCIPLINAS_TXT = "lista de disciplinas.txt";
+		private static final String HISTORICO_ESCOLAR_CR_APROVADOS_PDF = "historicoEscolarCRAprovados.pdf";
+		private static final Path LISTA_DISCIPLINA_PATH = Paths.get(System.getProperty("user.dir"), LISTA_DE_DISCIPLINAS_TXT);
+	    private static final Path HISTORIO_ESCOLAR_PATH = Paths.get(System.getProperty("user.dir"), HISTORICO_ESCOLAR_CR_APROVADOS_PDF);
+
+		public static void main(String[] args) throws IOException{
+	    	final File historicoEscolarDocumento = new File(HISTORIO_ESCOLAR_PATH.toString());        
+	    	String historicoEscolarExtraido = extrairHistoricoEscolar(historicoEscolarDocumento);
+	    	String historicoEscolarRefinado = refinadorDeConteudoDoHistoricoEscolar(historicoEscolarExtraido, 
+	    			recuperarIndexInicial(historicoEscolarDocumento), recuperarIndexFinal(historicoEscolarDocumento));
+	    	Disciplina.importarListaDisciplinas(LISTA_DISCIPLINA_PATH.toString());
+	    	Disciplina.encontrarStatusDeAprovacao(historicoEscolarRefinado);
+	    }
+		
+		
+		/**
+		 * Método utilizado para recuperar o index inicial do doc
+		 * @param historicoEscolarDoc
+		 * @return indexComeco
+		 * @throws IOException
+		 */
+		public static int recuperarIndexInicial(File historicoEscolarDoc) throws IOException  {
+			int indexComeco;
+			ArrayList<Integer> indexesComecoEFimDasMaterias = recuperarIndexesComecoEFim(historicoEscolarDoc); 
+			indexComeco = indexesComecoEFimDasMaterias.get(0);
+			return indexComeco;
+		}
+
+		
+		/**
+		 * Método utilizado para recuperar o index final do doc
+		 * @param historicoEscolarDoc
+		 * @return indexFim
+		 * @throws IOException
+		 */
+		public static int recuperarIndexFinal(File historicoEscolarDoc) throws IOException  {
+			int indexFim;   	
+			ArrayList<Integer> indexesComecoEFimDasMaterias = recuperarIndexesComecoEFim(historicoEscolarDoc); 
+			indexFim = indexesComecoEFimDasMaterias.get(1);
+			return indexFim;
+		}
+		
+		
+		/**
+		 * Método utilizado para recuperar os indexes de começo e de fim do doc
+		 * @param historicoEscolarDoc
+		 * @return indexesComecoEFimDasMaterias
+		 * @throws IOException
+		 */
+		private static ArrayList<Integer> recuperarIndexesComecoEFim(File historicoEscolarDoc) throws IOException {
+			ArrayList<Integer> indexesComecoEFimDasMaterias = new ArrayList<Integer>(); 
+			String historicoEscolarExtraido = extrairHistoricoEscolar(historicoEscolarDoc);
+			indexesComecoEFimDasMaterias = buscadoresDeIndex(historicoEscolarExtraido);
+			return indexesComecoEFimDasMaterias;
+		}
+
+		
+		/**
+		 * Método utilizado para extrarir o historico escolar do doc
+		 * @param historicoEscolarDoc
+		 * @return historicoEscolarExtraido
+		 * @throws IOException
+		 */
+		private static String extrairHistoricoEscolar(File historicoEscolarDoc) throws IOException {
+			String historicoEscolarExtraido;
+			historicoEscolarExtraido = extrairPDF(historicoEscolarDoc);
+			return historicoEscolarExtraido;
+		}
+		
+		
+	    
+	    /**
+	     * 
+	     * Este método procura extrair o histórico escolar do documento PDF e transformá-lo em uma String
+	     * 
+	     * @param historicoEscolarCaminho (File) : É o caminho de onde o histórico está armazenado na máquina. 
+	     * 
+	     * @return String com o histórico extraido
+	     * 
+	     * */
+
+	    public static String extrairPDF(File historicoEscolarCaminho) throws IOException {
+	        PDDocument historicoEscolar = PDDocument.load(historicoEscolarCaminho);        
+	        PDFTextStripper pdfStripper = new PDFTextStripper();
+	        String historicoEscolarExtraido = pdfStripper.getText(historicoEscolar);
+	        historicoEscolar.close();
+	        return historicoEscolarExtraido;
+	    }
+	    
+	    /**
+	     * Este método procura encontrar o início e o fim aproximado do conteúdo de matérias do histórico escolar.
+	     *  
+	     * @param historicoEscolarExtraido (String): String com o histórico escolar completo, antes do refinamento
+	     * 
+	     * @return : ArrayList<Integer> com os valores correspondentes às posições inicial e final do bloco de aprovações
+	     * 
+	     * */
+	        
+	    public static ArrayList<Integer> buscadoresDeIndex (String historicoEscolarExtraido) throws IOException {
+	    	ArrayList<Integer> indexesComecoEFimDasMaterias = new ArrayList<Integer>();
+	    	int indexComeco = historicoEscolarExtraido.indexOf("Situação Local");
+	    	int indexFim = historicoEscolarExtraido.indexOf("Coeficiente de Rendimento Geral");
+	    	
+	    	indexesComecoEFimDasMaterias.add(indexComeco);
+	    	indexesComecoEFimDasMaterias.add(indexFim);
+	    	
+	    	return indexesComecoEFimDasMaterias;
+	    }	    
+	    
+	    /**
+	     * Este método se destina a separar o bloco de aprovações do restante das informações do histórico
+	     * 
+	     * @param historicoEscolarExtraido (String) : String com o histórico escolar completo, antes do refinamento
+	     * @param indexComeco (Int) : Indicador do começo do texto refinado
+	     * @param indexFim (Int) : Indicador do fim do texto refinado
+	     * 
+	     * @return Substring com o bloco de matérias separado do restante
+	     * 
+	     * */
+	    
+	    public static String refinadorDeConteudoDoHistoricoEscolar(String historicoEscolarExtraido, int indexComeco, int indexFim){
+	  	    	
+	    	String historicoEscolarRefinado= historicoEscolarExtraido.substring(indexComeco, indexFim);   		    	
+	    	return historicoEscolarRefinado;
+	    }
+
+	    
+	    	    
+	    
+   }
+	
+
